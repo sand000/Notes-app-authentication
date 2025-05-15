65a81a82c31df6cb6974028b16ee189acb9c5133
Big Mac <ranjanakushwaha@Ranjanas-MacBook-Air.local>
2025-05-15 15:59:29 +0530

    client domain added in cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://animated-clafoutis-a8c97f.netlify.app/"],
   }),
 );
 
